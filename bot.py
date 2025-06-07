48f1c9f3d18cadda20f6c37648a6629055ee08c3
ravedante <ravedante6@gmail.com>
2025-06-06 22:07:19 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,27 @@ from keep_alive import keep_alive
 keep_alive()
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+import asyncio
 
 BOT_TOKEN = "8072106293:AAFZLW8HHoG8qHV9q_9iYzE4etVyVCDKaJM"
+WEBHOOK_URL = f"https://ravedantebot.onrender.com/{BOT_TOKEN}"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot online!")
+    await update.message.reply_text("🤖 Bot online via Webhook!")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+async def setup():
+    await app.bot.set_webhook(WEBHOOK_URL)
+
+asyncio.run(setup())
+
+PORT = int(os.environ.get("PORT", 8080))
+app.run_webhook(
+    listen="0.0.0.0",
+    port=PORT,
+    webhook_url=WEBHOOK_URL
+)
